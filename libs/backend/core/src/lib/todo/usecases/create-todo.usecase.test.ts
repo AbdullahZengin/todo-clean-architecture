1210264ec16096c6fc574232aae8a11be57fa386
Muammer TEKİN <muammertekiinpc@gmail.com>
2022-09-17 13:26:12 +0300

    test: add unit test to create todo usecase

---
+++ libs/backend/core/src/lib/todo/usecases/create-todo.usecase.test.ts
@@ -0,0 +1,54 @@
+import { Todo } from '../entities/todo.entity';
+import { ITodoRepository } from '../todo.repository.interface';
+import { CreateTodoUsecase } from './create-todo.usecase';
+
+const todoToCreate: Todo = {
+  id: '123',
+  body: 'This is new todo',
+  status: false,
+  createdDate: new Date(Date.now()),
+};
+
+class MockTodoRepository implements ITodoRepository {
+  getAll(): Promise<Todo[]> {
+    throw new Error('Method not implemented.');
+  }
+  getById(id: string): Promise<Todo> {
+    throw new Error('Method not implemented.');
+  }
+  async create(todo: Todo): Promise<Todo> {
+    return {
+      id: todo.id,
+      body: todo.body,
+      status: todo.status,
+      createdDate: new Date(Date.now()),
+    };
+  }
+  delete(id: string): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  update(todo: Partial<Todo>): Promise<Todo> {
+    throw new Error('Method not implemented.');
+  }
+}
+
+describe('Create todo usecase test', () => {
+  let repository: ITodoRepository;
+  let usecase: CreateTodoUsecase;
+
+  beforeEach(() => {
+    repository = new MockTodoRepository();
+    usecase = new CreateTodoUsecase(repository);
+  });
+
+  it('Creates a todo', async () => {
+    const createdTodo = await usecase.execute(todoToCreate);
+
+    expect(createdTodo).toBeDefined();
+
+    expect(createdTodo.id).toEqual(todoToCreate.id);
+    expect(createdTodo.body).toEqual(todoToCreate.body);
+    expect(createdTodo.status).toEqual(todoToCreate.status);
+    
+   });
+});
