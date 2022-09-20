0601c4a2718d78b4184c16beb8104f5ffb62e964
Giizemm <gizemmunaal@gmail.com>
2022-09-20 16:10:03 +0300

    test: write unit test for todo repository

--- libs/presentation/data/src/lib/todo/todo.repository.test.ts
+++ libs/presentation/data/src/lib/todo/todo.repository.test.ts
@@ -2,8 +2,34 @@ import { Todo } from '@udao/presentation-core';
 import { ITodoApiDatasource } from './datasources/api/todo-api-datasource.interface';
 import { TodoRepository } from './todo.repository';
 
+const todos: Todo[] = [
+  {
+    id: '1',
+    body: 'Foo',
+    createdDate: new Date(Date.now()),
+    status: true,
+  },
+  {
+    id: '2',
+    body: 'Foo',
+    createdDate: new Date(Date.now()),
+    status: true,
+  },
+  {
+    id: '3',
+    body: 'Foo',
+    createdDate: new Date(Date.now()),
+    status: true,
+  },
+  {
+    id: '4',
+    body: 'Foo',
+    createdDate: new Date(Date.now()),
+    status: false,
+  },
+];
 class MockDatasource implements ITodoApiDatasource {
-  private storage: Todo[] = [];
+  private storage: Todo[] = todos;
 
   async getAllTodos(): Promise<Todo[]> {
     return this.storage;
@@ -18,9 +44,7 @@ class MockDatasource implements ITodoApiDatasource {
   async updateTodo(todo: Todo): Promise<Todo> {
     const todoIndex = this._findTodoByIndex(todo.id);
 
-    this.storage[todoIndex] = todo;
-
-    return todo;
+    return (this.storage[todoIndex] = todo);
   }
 
   async deleteTodo(id: string): Promise<void> {
@@ -42,14 +66,59 @@ describe('TodoRepository', () => {
   let todoRepository: TodoRepository;
   let datasource: ITodoApiDatasource;
 
-  let todos: Todo[];
-
   beforeEach(() => {
     datasource = new MockDatasource();
     todoRepository = new TodoRepository(datasource);
   });
 
-  it('hlp me', () => {
-    expect(todoRepository).toBeDefined();
+  it('Get all todo', async () => {
+    const allTodo = await todoRepository.getAllTodos();
+
+    expect(allTodo).toBeDefined();
+    expect(Array.isArray(allTodo)).toBeTruthy();
+    expect(allTodo.length).toBe(4);
+  });
+
+  it('Create todo', async () => {
+    const todoToCreate: Todo = {
+      id: '1',
+      body: 'New todo',
+      status: false,
+      tag: 'tag 1',
+      createdDate: new Date(Date.now()),
+    };
+
+    const createdTodo = await todoRepository.createTodo(todoToCreate);
+
+    expect(createdTodo).toBeDefined();
+    expect(Object.keys(createdTodo).length).toEqual(
+      Object.keys(todoToCreate).length
+    );
+    expect(createdTodo).toEqual(todoToCreate);
+  });
+
+  it('Update todo', async () => {
+    const todoToUpdate: Todo = {
+      id: todos[0].id,
+      body: 'Updated body',
+      status: todos[0].status,
+      createdDate: todos[0].createdDate,
+    };
+
+    const updatedTodo = await todoRepository.updateTodo(todoToUpdate);
+
+    expect(updatedTodo).toBeDefined();
+    expect(Object.keys(updatedTodo).length).toEqual(Object.keys(todoToUpdate).length);
+    expect(updatedTodo).toEqual(todoToUpdate);
+  });
+
+  it('Toggle todo status', async () => {
+    await expect(
+      todoRepository.toggleTodoStatus(todos[0].id)
+    ).resolves.not.toThrow();
+  });
+
+  it('Delete todo status', async () => {
+    await expect(todoRepository.deleteTodo(todos[0].id)).resolves.not.toThrow();
   });
 });
